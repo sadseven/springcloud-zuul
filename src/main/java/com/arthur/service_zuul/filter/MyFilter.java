ed57808165bfd8e941b6736b49cf2c8edb62a94b
sadSeven <961900710@qq.com>
2018-10-15 10:28:23 +0800

    路由网关(zuul)

---
+++ src/main/java/com/arthur/service_zuul/filter/MyFilter.java
@@ -0,0 +1,42 @@
+package com.arthur.service_zuul.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class MyFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }
+        return null;
+    }
+}
